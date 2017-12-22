d82aee9996607a8931456f0cfc8c888002ccc1be
Nicolás J. Engler <nicojengler@gmail.com>
2017-12-22 15:28:24 -0300

    Finish desktop & tablet versions

--- src/scripts/fullpage-control.js
+++ src/scripts/fullpage-control.js
@@ -8,13 +8,13 @@ $(document).ready(function() {
   };
 
   $('#fullpage').fullpage({
-    //Navigation
+    // Navigation
     lockAnchors: false,
     anchors:['intro', 'partnership', 'strengths', 'experimentation', 'contact'],
     navigation: false,
     slidesNavigation: false,
 
-    //Scrolling
+    // Scrolling
     css3: true,
     scrollingSpeed: 1000,
     autoScrolling: true,
@@ -41,33 +41,53 @@ $(document).ready(function() {
     normalScrollElementTouchThreshold: 5,
     bigSectionsDestination: null,
 
-    //Accessibility
+    // Accessibility
     keyboardScrolling: true,
     animateAnchor: true,
     recordHistory: true,
 
-    //Design
+    // Design
     controlArrows: false,
     verticalCentered: true,
-    fixedElements: '.navigation, .grid-ornament, .nav-button',
+    fixedElements: '.navigation, .grid-ornament, .nav-button, .animated-box',
     responsiveSlides: false,
     parallax: false,
     parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
 
-    //Custom selectors
+    // Custom selectors
     sectionSelector: '.section',
     slideSelector: '.slide',
 
     lazyLoading: true,
 
-    //events
-    onLeave: function(index, nextIndex, direction){},
-    afterLoad: function(anchorLink, index){},
-    afterRender: function(){},
-    afterResize: function(){},
-    afterResponsive: function(isResponsive){},
-    afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
-    onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+    // Events
+    onLeave: function(index, nextIndex, direction) {
+      var leavingSection = $(this),
+          $animatedBox = $('.js-animated-box');
+
+	  // After leaving intro
+      if (nextIndex == 1) {
+        $animatedBox.removeClass('is-shown');
+      } else {
+        $animatedBox.addClass('is-shown');
+      }
+    },
+    afterLoad: function(anchorLink, index) {
+      var loadedSection = $(this),
+          $animatedBox = $('.js-animated-box');
+
+	  // Using anchorLink
+	  if (anchorLink != 'intro') {
+	    $animatedBox.removeClass('delay-animation');
+      } else {
+        $animatedBox.addClass('delay-animation');
+      }
+    },
+    afterRender: function() {},
+    afterResize: function() {},
+    afterResponsive: function(isResponsive) {},
+    afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex) {},
+    onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex) {}
   });
     
   $('.js-scroll-button').on('click', scrollSection);
